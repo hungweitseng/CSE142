03594b3a235543d137d472046ea16a100b674e6d
hunweitseng <h1tseng@cs.ucsd.edu>
2022-08-16 09:35:30 -0700

    Memory demos

---
+++ memory/alignment/struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct movie
+{
+  int id; // 4B
+  int participation; // 4B
+  double *homework; // 8B
+  double midterm; // 8B
+  double average; // 8B
+};
+      
+int size=65533;
+int max=65538;
+
+int main(int argc, char **argv)
+{
+  int i;
+  struct movie *movies;
+  movies = malloc(sizeof(struct movie)*1);
+/*  for(i=0;i<size;i++)
+  {
+    movies[i].id = rand();
+    movies[i].year = 2016;
+    movies[i].sold = 0.0;
+    movies[i].viewers = 3000;
+    movies[i].score = 0.0;          
+  }*/
+    printf("%lu\n",sizeof(struct movie));
+    printf("%p %p %p %p %p\n",&movies[0].id, &movies[0].homework, &movies[0].participation, &movies[0].midterm, &movies[0].average);
+//  printf("Hello, world! %d %lf\nHow big is a \"movie?\" %lu\n",movies[rand()%size].id,movies[rand()%size].score,sizeof(struct movie));
+  return 0;
+}
